0a8e7d6fe440603d523a44ce4757b9064dd4ada1
Alexis Chilinski <chilinski.a@gmail.com>
2019-10-22 18:16:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,8 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |describers, details|
     details.each do |features, name|
-      
+      if pigeon_list[name] = nil 
+        pigeon_list[name] = {}
+        pigeon_list
     end
 end
